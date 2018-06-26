35d65979ed2d097f1776670e4e17d86de5981776
orlo wang <ow.cc@outlook.com>
2018-06-26 16:31:27 +0800

    init

---
+++ rollup.config.js
@@ -0,0 +1,82 @@
+import resolve from 'rollup-plugin-node-resolve'
+import replace from 'rollup-plugin-replace'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+import { sizeSnapshot } from "rollup-plugin-size-snapshot"
+import pkg from './package.json'
+
+const banner = `
+/* ${pkg.name} version ${pkg.version}
+ *
+ * Copyright (c) 2013-present, ${pkg.author}
+ * 
+ * This source code is licensed under the ${pkg.license} license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+`
+
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      file: `lib/es/${pkg.name}.js`,
+      name: `${pkg.name}`,
+      format: 'es',
+      banner,
+      globals: {
+        react: 'React',
+        '@storybook/addons': 'addons',
+        'prop-types': 'string'
+      }
+    },
+    external: ['react', 'prop-types', '@storybook/addons'],
+    plugins: [
+      resolve(),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+      babel({
+        exclude: 'node_modules/**',
+      }),
+      sizeSnapshot(),
+      terser({
+        output: {
+          comments: true,
+          beautify: true,
+        },
+      })
+    ]
+  },
+  {
+    input: 'src/index.js',
+    output: {
+      file: `lib/cjs/${pkg.name}.js`,
+      name: `${pkg.name}`,
+      format: 'cjs',
+      banner,
+      globals: {
+        react: 'React',
+        'styled-components': 'styled',
+        'prop-types': 'string'
+      },
+      exports: 'named'
+    },
+    external: ['react', 'prop-types', 'styled-components', 'react-is'],
+    plugins: [
+      resolve({
+        preferBuiltins: true
+      }),
+      replace({ 'process.env.NODE_ENV': JSON.stringify('development') }),
+      babel({
+        exclude: 'node_modules/**',
+      }),
+      commonjs(),
+      sizeSnapshot(),
+      terser({
+        output: {
+          comments: true,
+          beautify: true,
+        },
+      })
+    ]
+  }
+]
